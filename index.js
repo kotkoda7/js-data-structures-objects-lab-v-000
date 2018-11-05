450c4f734940ad71f524bdb3cdd587648451c907
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-05 21:39:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 function deleteFromDriverByKey(driver, key) {
   const newObj = Object.assign({}, driver);
   
-  
+  delete newObj[key];
+
+  return newObj;
 }
+
